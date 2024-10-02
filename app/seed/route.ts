661e24326a634330358b7fc52cc23b1e1f514729
[chishimab chasanda] <[chichichasnda@gmail.com]>
2024-10-02 09:46:54 +0200

    Managed to seed the db from app\seed\routes.ts I will just comment out this file will not delete it.

--- app/seed/route.ts
+++ app/seed/route.ts
@@ -69,43 +69,41 @@
 
    const insertedCustomers = await Promise.all(
      customers.map(
-//       (customer) => client.sql`
-//         INSERT INTO customers (id, name, email, image_url)
-//         VALUES (${customer.id}, ${customer.name}, ${customer.email}, ${customer.image_url})
-//         ON CONFLICT (id) DO NOTHING;
-//       `,
-//     ),
-//   );
+       (customer) => client.sql`
+         INSERT INTO customers (id, name, email, image_url)
+         VALUES (${customer.id}, ${customer.name}, ${customer.email}, ${customer.image_url})
+         ON CONFLICT (id) DO NOTHING;
+       `,
+     ),
+   );
+   return insertedCustomers;
+ }
 
-//   return insertedCustomers;
-// }
+ async function seedRevenue() {
+   await client.sql`
+     CREATE TABLE IF NOT EXISTS revenue (
+       month VARCHAR(4) NOT NULL UNIQUE,
+       revenue INT NOT NULL
+     );
+   `;
 
-// async function seedRevenue() {
-//   await client.sql`
-//     CREATE TABLE IF NOT EXISTS revenue (
-//       month VARCHAR(4) NOT NULL UNIQUE,
-//       revenue INT NOT NULL
-//     );
-//   `;
-
-//   const insertedRevenue = await Promise.all(
-//     revenue.map(
-//       (rev) => client.sql`
-//         INSERT INTO revenue (month, revenue)
-//         VALUES (${rev.month}, ${rev.revenue})
-//         ON CONFLICT (month) DO NOTHING;
-//       `,
-//     ),
-//   );
-
-//   return insertedRevenue;
+   const insertedRevenue = await Promise.all(
+     revenue.map(
+       (rev) => client.sql`
+         INSERT INTO revenue (month, revenue)
+         VALUES (${rev.month}, ${rev.revenue})
+         ON CONFLICT (month) DO NOTHING;
+       `,
+     ),
+   );
+   return insertedRevenue;
  }
 
 export async function GET() {
-  return Response.json({
-    message:
-      'Uncomment this file and remove this line. You can delete this file when you are finished.',
-  });
+ // return Response.json({
+   // message:
+    //  'Uncomment this file and remove this line. You can delete this file when you are finished.',
+  //});
    try {
      await client.sql`BEGIN`;
      await seedUsers();
