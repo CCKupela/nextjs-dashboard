50f6bab85eaf37afd435ff1583781969618579a1
[chishimab chasanda] <[chichichasnda@gmail.com]>
2024-10-03 08:09:17 +0200

    yesterday's work

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -5,10 +5,9 @@
 import { Card } from '@/app/ui/dashboard/cards';
 import RevenueChart from '@/app/ui/dashboard/revenue-chart';
 import LatestInvoices from '@/app/ui/dashboard/latest-invoices';
-import { lusitana } from '@/app/ui/fonts';
-import { fetchRevenue } from '../lib/data';
-import { fetchLatestInvoices } from '../lib/data';
-import { fetchCardData } from '../lib/data';
+import { lusitana, } from '@/app/ui/fonts';
+ import { fetchCardData, fetchLatestInvoices, fetchRevenue } from '../lib/data';
+
 
 
  
@@ -20,7 +19,7 @@ export default async function Page() {
         numberOfCustomers,
         totalPaidInvoices,
         totalPendingInvoices,
-      } = await fetchCardData();
+      } = await fetchCardData ();
   return (
     <main>
       <h1 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
